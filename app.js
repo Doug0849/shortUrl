21f1db08efaacc232fcbaaefb4941910c5124c54
doug0849 <etaomonk@gmail.com>
2022-02-02 01:17:42 +0800

    連線mongodb資料庫

--- app.js
+++ app.js
@@ -4,10 +4,20 @@ const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/short-url')
 
 const app = express()
-const PORT = 3000
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('mongodb error')
+})
 
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+const PORT = 3000
 app.engine('hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs'}))
 app.set('view engine', 'hbs')
+
+
 
 app.get('/', (req, res) => {
   res.send('hello world!')
